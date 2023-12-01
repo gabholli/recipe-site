e4ef2a0f5f395063468669755271af0d0527471e
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-11-30 23:20:26 -0800

    Made many changes
    Add beginnings to component files and started implementing routes in App.js.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+
+export default function Header() {
+
+    return (
+        <header>
+            <Link to="/">Home</Link>
+            <nav>
+            </nav>
+        </header>
+    )
+}

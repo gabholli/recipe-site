92049d393eb1e33b27b68e4d155674643933e0b3
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-01 17:02:37 -0800

    Removed periods from sentences in title page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
 
     return (
         <div className="home-container">
-            <h1 className="home-heading">All the recipes you need.</h1>
-            <p className="home-subtext">We should have the information you're looking for.</p>
+            <h1 className="home-heading">All the recipes you need</h1>
+            <p className="home-subtext">We should have the information you're looking for</p>
         </div>
     )
 }

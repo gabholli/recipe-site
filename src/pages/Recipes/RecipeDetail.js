62f1f478f8ed99a58dfaaf335e86f58686d4f900
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-02 19:03:17 -0800

    Made several changes, mostly in css
    Added elements from API to detail screen and styled them.

--- src/pages/Recipes/RecipeDetail.js
+++ src/pages/Recipes/RecipeDetail.js
@@ -1,8 +1,39 @@
 import React from "react"
-import { Link, useParams, useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
 
 export default function RecipeDetail() {
+    const params = useParams()
+    const [food, setFood] = useState([])
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error("Data not available")
+                }
+                return response.json()
+            })
+            .then(data => {
+                setFood(data.meals)
+            })
+            .catch(error =>
+                console.log("Fetch error: ", error))
+    }, [params.id])
+
+    const foodElements = food.map(item => {
+        return (
+            <div className="recipe-details-container">
+                <h1 className="detail-name">{item.strMeal}</h1>
+                <img className="detail-image" src={item.strMealThumb} alt="Food item" />
+                <p className="recipe-instructions">{item.strInstructions}</p>
+            </div>
+        )
+    })
+
     return (
-        <h1>Details go here.</h1>
+        <div className="detail-container">
+            {foodElements}
+        </div>
     )
 }

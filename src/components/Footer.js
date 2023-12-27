d02d4edcb7ee8a4cdf3fd6b2448126a9945d1820
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-27 11:17:42 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer className="h 1/3 bg-gradient-to-b from-green-200 to-green-300 text-base 
+        <footer className="h-1/3 bg-gradient-to-b from-green-200 to-green-300 text-base 
             sm:text-4xl text-center flex justify-center items-center">
             &#169;2023 Recipe Site
         </footer>

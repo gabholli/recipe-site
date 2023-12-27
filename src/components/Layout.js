906a44eb05a3a7e52c9a4d5ea482af4fdca1f2ce
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-27 12:03:50 -0800

    Changed layout container back to flex

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 
 export default function Layout() {
     return (
-        <div className="font-hedvig text-gray-800 grid grid-rows-[auto_1fr_auto] min-h-screen">
+        <div className="font-hedvig text-gray-800 flex flex-col min-h-screen">
             <Header />
             <Outlet />
             <Footer />

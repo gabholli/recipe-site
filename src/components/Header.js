d6f6fd8b5cd79122126cdc3598c890e397d71c88
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-26 22:22:20 -0800

    Added green gradient in several places
    Added green gradient to header, footer, and form elements.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
 
 
     return (
-        <header className="h-20 bg-green-200 text-2xl sm:text-4xl text-center flex justify-center items-center">
+        <header className="h-20 bg-gradient-to-b from-green-200 to-green-300 text-2xl sm:text-4xl text-center flex justify-center items-center">
 
             <nav className="text-2xl sm:text-4xl flex justify-center items-center gap-6 m-4">
                 <NavLink
--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -61,7 +61,8 @@ export default function Recipes() {
         <div className="bg-green-100 py-12 flex flex-1 flex-col items-center">
             <form className="text-center mb-12 flex flex-col sm:flex-row sm:items-center 
             justify-center gap-4" onSubmit={handleSubmit}>
-                <input className="border-2 border-gray-500 rounded-lg indent-3 h-10 w-64"
+                <input className="bg-gradient-to-b from-green-200 to-green-300
+                border-2 border-gray-500 rounded-lg indent-3 h-10 w-64"
                     type="text"
                     placeholder="Enter dish or ingredient name"
                     onChange={handleChange}
@@ -69,7 +70,8 @@ export default function Recipes() {
                     value={foodItemFromLocalStorage || ""}
                 >
                 </input>
-                <button className="bg-green-200 border-2 border-gray-500 rounded-lg h-10 px-4 py-2">Search</button>
+                <button className="bg-gradient-to-b from-green-200 to-green-300
+                    border-2 border-gray-500 rounded-lg h-10 px-4 py-2">Search</button>
             </form>
             {foodData ? (
                 <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-12 mx-12">

d39ed44cf85110613cc8ab37b9e1175bdfc03fd7
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-26 22:46:00 -0800

    Made link sizes smaller for mobile screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer className="h-20 bg-gradient-to-b from-green-200 to-green-300 text-2xl sm:text-4xl text-center flex justify-center items-center">
+        <footer className="h-20 bg-gradient-to-b from-green-200 to-green-300 text-base 
+            sm:text-4xl text-center flex justify-center items-center">
             &#169;2023 Recipe Site
         </footer>
     )

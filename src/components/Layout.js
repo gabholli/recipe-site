c1d68aae81b6aa57e7dbda68c962c9d061b0e78f
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-12-28 23:23:44 -0800

    Change text color across whole site to black

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 
 export default function Layout() {
     return (
-        <div className="font-hedvig text-gray-800 flex flex-col min-h-svh">
+        <div className="font-hedvig text-black flex flex-col min-h-svh">
             <Header />
             <Outlet />
             <Footer />

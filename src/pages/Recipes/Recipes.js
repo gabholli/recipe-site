5f8fba69067800cee181598c0821be6150f7c183
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-29 21:00:11 -0800

    Updated project to React Router 7

--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router"
 
 export default function Recipes() {
     const foodItemFromLocalStorage = JSON.parse(localStorage.getItem("foodItem"))

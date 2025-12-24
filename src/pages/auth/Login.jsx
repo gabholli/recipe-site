cf250badeb9964b10bb8fb25c7206054792495a1
Gabe Hollifield <gholl467@gmail.com>
2025-12-24 13:22:40 -0800

    Update Login.jsx

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -7,7 +7,7 @@ export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
+    // const [loading, setLoading] = useState(false)
 
     const { signInUser } = UserAuth()
     const navigate = useNavigate()

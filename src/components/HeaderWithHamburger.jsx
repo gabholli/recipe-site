345e614d35d6d28166d4408b44e31008fd3f3354
Gabe Hollifield <gholl467@gmail.com>
2026-03-31 12:21:55 -0700

    Added hamburger menu for mobile view

---
+++ src/components/HeaderWithHamburger.jsx
@@ -0,0 +1,112 @@
+import { NavLink } from "react-router";
+import { UserAuth } from "../context/AuthContext";
+import { Menu } from '@boxicons/react';
+import { useState } from "react";
+
+export default function HeaderWithHamburger() {
+
+    const { session, signOut } = UserAuth()
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <header className="relative flex justify-end lg:justify-center items-center bg-opacity-50 bg-neutral-600
+            text-white py-6 px-8 md:px-16 xl:px-32 text-center">
+            <nav className="hidden lg:flex items-center gap-12
+                    md:text-3xl">
+                <NavLink
+                    className="hover:underline"
+                    to="/">Home
+                </NavLink>
+                <NavLink
+                    to="/recipes"
+                    className="hover:underline"
+                >
+                    Recipes
+                </NavLink>
+                <NavLink
+                    to="/random"
+                    className="hover:underline"
+                >
+                    Random Meal
+                </NavLink>
+                <NavLink
+                    to="/favorites"
+                    className="hover:underline"
+                >
+                    Favorites
+                </NavLink>
+                {!session && (<NavLink
+                    to="/signup"
+                    className="hover:underline"
+                >
+                    Sign Up/Log In
+                </NavLink>
+                )
+                }
+                {session && (<button
+                    className="hover:underline"
+                    onClick={signOut}
+                >
+                    Sign Out
+                </button>
+                )
+                }
+            </nav>
+
+            <div
+                onClick={() => setIsOpen(open => !open)}
+                className="lg:hidden block cursor-pointer">
+                <Menu />
+            </div>
+
+            <div className={`absolute xl:hidden top-full
+                    left-0 w-full bg-neutral-900
+                    flex-col items-center gap-6 font-semibold
+                    transform transition-transform
+                    ${isOpen ? "flex" : "hidden"}`}>
+                <nav className="w-full text-center
+                        p-4 hover:bg-neutral-800 transition-all
+                        cursor-pointer flex flex-col gap-y-6">
+                    <NavLink
+                        className="hover:underline"
+                        to="/">Home
+                    </NavLink>
+                    <NavLink
+                        to="/recipes"
+                        className="hover:underline"
+                    >
+                        Recipes
+                    </NavLink>
+                    <NavLink
+                        to="/random"
+                        className="hover:underline"
+                    >
+                        Random Meal
+                    </NavLink>
+                    <NavLink
+                        to="/favorites"
+                        className="hover:underline"
+                    >
+                        Favorites
+                    </NavLink>
+                    {!session && (<NavLink
+                        to="/signup"
+                        className="hover:underline"
+                    >
+                        Sign Up/Log In
+                    </NavLink>
+                    )
+                    }
+                    {session && (<button
+                        className="hover:underline"
+                        onClick={signOut}
+                    >
+                        Sign Out
+                    </button>
+                    )
+                    }
+                </nav>
+            </div>
+        </header>
+    )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router"
-import Header from "./Header"
 import Footer from "./Footer"
 import BackToTopButton from "./BackToTopButton"
+import HeaderWithHamburger from "./HeaderWithHamburger"
 
 export default function Layout() {
     return (
         <div className="bg-black font-hedvig text-white
             grid grid-rows-[auto_1fr_auto] grid-cols-1 w-full min-h-screen">
-            <Header />
+            <HeaderWithHamburger />
+            {/* <Header /> */}
             <Outlet />
             <BackToTopButton />
             <Footer />
